cb3dfdbbecb693f16d39d074bc59ebb65161df76
Arpi <arpad.lengyel@around25.com>
2024-08-25 23:10:33 +0300

    remove unused

--- frontend/app/lib/webExtesnsionClient.ts
+++
@@ -1,8 +0,0 @@
-export class WebExtensionClient {
-  public static async triggerWebExtensionWindow(e: React.MouseEvent<HTMLElement>): Promise<void> {
-    e.preventDefault()
-    
-    // we need some logic here when we have web extension exposed function for triggering window
-    alert("clicked")
-  }
-}
--- frontend/app/routes/embed.tsx
+++
@@ -1,7 +0,0 @@
-export default function Embed() {
-    return (
-      <div className="bg-gradient-to-r from-wm-green to-wm-green-fade border-t-4 border-t-wm-green-shade p-12">
-        
-      </div>
-    )
-  }
