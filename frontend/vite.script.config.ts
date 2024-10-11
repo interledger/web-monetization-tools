2e7d1976a296a74093ac1fdc986bdbebcde34eae
Adrian Boros <adiboros@hotmail.com>
2024-10-11 13:55:40 +0300

    add watchers and rebuild for backend, frontend and frontend script for docker dev setup env

--- frontend/vite.script.config.ts
+++ frontend/vite.script.config.ts
@@ -1,20 +1,32 @@
 // vite.script.config.ts
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  build: {
-    lib: {
-      entry: './script/index.ts',  // Entry point for the script file
-      formats: ['iife'],  // Use IIFE format (can change based on your needs)
-      name: 'InitScript',
-      fileName: () => 'init.js',
+export default defineConfig(({ mode }) => {
+  const isDevelopment = mode === 'development';
+
+  return {
+    build: {
+      lib: {
+        entry: './script/index.ts',  // Entry point for the script file
+        formats: ['iife'],  // Use IIFE format (can change based on your needs)
+        name: 'InitScript',
+        fileName: () => 'init.js',
+      },
+      outDir: './public',  // Output directory
+      emptyOutDir: false,  // Don't clear the directory when building
+      rollupOptions: {
+        output: {
+          entryFileNames: 'init.js',  // Name of the output file
+        }
+      },
+      watch: isDevelopment ? {} : null, 
     },
-    outDir: './public',  // Output directory
-    emptyOutDir: false,  // Don't clear the directory when building
-    rollupOptions: {
-      output: {
-        entryFileNames: 'init.js',  // Name of the output file
-      }
-    }
+    server: {
+      watch: {
+        usePolling: true, // Ensures file polling for file changes
+        ignored: ['node_modules/**', 'public/**'], // Ignore node_modules and output directory
+      },
+    },
   }
+
 });
