1db4ca0e27c5f3b7432ed2dcd2f9e34c39a35645
Arpi <arpad.lengyel@breakpointit.eu>
2025-03-11 00:23:05 +0200

    query parameter added for conditional display of header & footer

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,4 +1,5 @@
-import type { MetaFunction } from '@remix-run/node'
+import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node'
+import { json, useLoaderData } from '@remix-run/react'
 import { TypeCard } from '~/components/index.js'
 import { availableTools } from '~/lib/presets.js'
 
@@ -9,13 +10,28 @@ export const meta: MetaFunction = () => {
   ]
 }
 
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url)
+  const contentOnlyParam = url.searchParams.get('contentOnly')
+
+  return json({ contentOnlyParam })
+}
+
 export default function Index() {
+  const { contentOnlyParam } = useLoaderData<typeof loader>()
+  const contentOnly = contentOnlyParam != null
+
   return (
     <div className="h-full p-14">
       <div className="flex flex-wrap justify-center gap-6 max-w-prosex p-4 mx-auto">
-        {availableTools.map((tool) => (
-          <TypeCard key={tool.link} {...tool} />
-        ))}
+        {availableTools.map((tool) => {
+          const toolLink = `${tool.link}${contentOnly && '?contentOnly'}`
+          const props = {
+            ...tool,
+            link: toolLink
+          }
+          return <TypeCard key={toolLink} {...props} />
+        })}
       </div>
     </div>
   )
