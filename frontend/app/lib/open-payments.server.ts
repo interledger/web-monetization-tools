961b3830a9985754ca5a11eb28d5125f48931424
Arpi <arpad.lengyel@breakpointit.eu>
2025-03-17 16:45:24 +0200

    cleanup OP functions

--- frontend/app/lib/open-payments.server.ts
+++ frontend/app/lib/open-payments.server.ts
@@ -97,42 +97,6 @@ export async function fetchQuote({
   return response
 }
 
-export async function fetchRequestQuote(args: {
-  walletAddress: string
-  incomingPaymentUrl: string
-}) {
-  const opClient = await createClient()
-  const walletAddress = await getWalletAddress(args.walletAddress, opClient)
-
-  const quoteGrant = await getQuoteGrant({
-    authServer: walletAddress.authServer,
-    opClient: opClient
-  })
-
-  if (isPendingGrant(quoteGrant)) {
-    throw new Error('Expected non-interactive grant')
-  }
-
-  // create quote with amount details in incoming payment url
-  const quote = await opClient.quote
-    .create(
-      {
-        url: new URL(walletAddress.id).origin,
-        accessToken: quoteGrant.access_token.value
-      },
-      {
-        method: 'ilp',
-        walletAddress: walletAddress.id,
-        receiver: args.incomingPaymentUrl
-      }
-    )
-    .catch(() => {
-      throw new Error(`Could not create quote for request payment.`)
-    })
-
-  return quote
-}
-
 type CreateIncomingPaymentParams = {
   accessToken: string
   walletAddress: WalletAddress
@@ -166,60 +130,17 @@ async function createIncomingPayment({
     })
 }
 
-export async function createRequestPayment(args: {
-  walletAddress: string
-  amount: number
-  note?: string
-}) {
-  const opClient = await createClient()
-  const walletAddress = await getWalletAddress(args.walletAddress, opClient)
-
-  const amountObj = {
-    value: BigInt(
-      (args.amount * 10 ** walletAddress.assetScale).toFixed()
-    ).toString(),
-    assetCode: walletAddress.assetCode,
-    assetScale: walletAddress.assetScale
-  }
-
-  const incomingPaymentGrant = await getIncomingPaymentGrant(
-    walletAddress.authServer,
-    opClient
-  )
-
-  // create incoming payment with amount
-  return await opClient.incomingPayment
-    .create(
-      {
-        url: new URL(walletAddress.id).origin,
-        accessToken: incomingPaymentGrant.access_token.value
-      },
-      {
-        expiresAt: new Date(Date.now() + 6000 * 60 * 5).toISOString(),
-        walletAddress: walletAddress.id,
-        incomingAmount: amountObj,
-        metadata: {
-          description: args.note
-        }
-      }
-    )
-    .catch(() => {
-      throw new Error('Unable to create incoming payment for request.')
-    })
-}
-
-export async function initializePayment(args: {
-  walletAddress: string
+export async function createValidationPayment(args: {
+  walletAddress: WalletAddress
   quote: Quote
   redirectUrl?: string
 }) {
   const opClient = await createClient()
-  const walletAddress = await getWalletAddress(args.walletAddress, opClient)
   const clientNonce = randomUUID()
   const paymentId = createId()
 
   const outgoingPaymentGrant = await createOutgoingPaymentGrant({
-    walletAddress: walletAddress,
+    walletAddress: args.walletAddress,
     debitAmount: args.quote.debitAmount,
     receiveAmount: args.quote.receiveAmount,
     nonce: clientNonce,
@@ -300,7 +221,7 @@ async function createOutgoingPaymentGrant(
   return grant
 }
 
-export async function finishPayment(
+export async function finishValidationPayment(
   payment: PendingGrant,
   quote: Quote,
   walletAddress: WalletAddress,
@@ -334,11 +255,12 @@ export async function finishPayment(
         walletAddress: walletAddress.id,
         quoteId: quote.id,
         metadata: {
-          description: 'Payment at Interledger Pay'
+          description: 'Publisher Tools wallet validated'
         }
       }
     )
-    .catch(() => {
+    .catch((error) => {
+      console.log({ error })
       throw new Error('Could not create outgoing payment.')
     })
 
@@ -346,97 +268,6 @@ export async function finishPayment(
     url: outgoingPayment.id,
     accessToken: continuation.access_token.value
   }
-}
-
-function timeout(delay: number) {
-  return new Promise((res) => setTimeout(res, delay))
-}
-
-export type PaymentResultType = {
-  message: string
-  color: 'red' | 'green'
-  error: boolean
-}
-
-export async function checkOutgoingPayment(
-  url: string,
-  accessToken: string,
-  accessTokenIncomingPayment: string,
-  receiver: string,
-  isRequestPayment?: boolean
-): Promise<PaymentResultType> {
-  const opClient = await createClient()
-  await timeout(3000)
-
-  // get outgoing payment, to check if there was enough balance
-  const checkOutgoingPaymentResponse = await opClient.outgoingPayment
-    .get({
-      url: url,
-      accessToken: accessToken
-    })
-    .then((op: { sentAmount: { value: string } }) => {
-      let paymentResult: PaymentResultType
-      if (Number(op.sentAmount.value) > 0) {
-        paymentResult = {
-          message: 'Payment successful',
-          color: 'green',
-          error: false
-        }
-      } else {
-        paymentResult = {
-          message: 'Payment failed. Check your balance and try again.',
-          color: 'red',
-          error: true
-        }
-      }
-
-      return paymentResult
-    })
-
-  if (!checkOutgoingPaymentResponse.error) {
-    if (!isRequestPayment) {
-      // complete incoming payment created without receive amount
-      await opClient.incomingPayment
-        .complete({
-          url: receiver,
-          accessToken: accessTokenIncomingPayment
-        })
-        .catch(() => {
-          throw new Error('Could not complete incoming payment.')
-        })
-    }
-  }
-
-  return checkOutgoingPaymentResponse
-}
-
-export async function getRequestPaymentDetails(
-  paymentUrl: string,
-  walletAddress: string
-) {
-  const opClient = await createClient()
-
-  const receiver = await getWalletAddress(walletAddress, opClient)
-  const incomingPaymentGrant = await getIncomingPaymentGrant(
-    receiver.authServer,
-    opClient
-  )
-
-  const paymentDetails = await opClient.incomingPayment
-    .get({
-      url: paymentUrl,
-      accessToken: incomingPaymentGrant.access_token.value
-    })
-    .catch(() => {
-      throw new Error('Could not retrieve payment details.')
-    })
-
-  return paymentDetails
-}
-
-type QuoteGrantParams = {
-  authServer: string
-  opClient: AuthenticatedClient
 }
 
 export async function getWalletAddress(
@@ -454,6 +285,11 @@ export async function getWalletAddress(
     })
 
   return walletAddress
+}
+
+type QuoteGrantParams = {
+  authServer: string
+  opClient: AuthenticatedClient
 }
 
 async function getQuoteGrant({ authServer, opClient }: QuoteGrantParams) {
