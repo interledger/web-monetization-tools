39b4fbaf0e86ba67dd903114c013e1110dc02e65
darian <darian410@gmail.com>
2025-05-07 13:14:44 +0300

    format

--- embed/env.d.ts
+++ embed/env.d.ts
@@ -1,11 +1,11 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_SCRIPT_FRONTEND_URL: string;
-    readonly VITE_SCRIPT_API_URL: string;
-    readonly VITE_SCRIPT_ILPAY_URL: string;
-  }
-  
-  interface ImportMeta {
-    readonly env: ImportMetaEnv;
-  }
+  readonly VITE_SCRIPT_FRONTEND_URL: string
+  readonly VITE_SCRIPT_API_URL: string
+  readonly VITE_SCRIPT_ILPAY_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
--- embed/src/worker.ts
+++ embed/src/worker.ts
@@ -1,42 +1,45 @@
-import type { ExecutionContext } from '@cloudflare/workers-types';
+import type { ExecutionContext } from '@cloudflare/workers-types'
 
 export interface Env {
   ASSETS: {
-    fetch: typeof fetch;
-  };
+    fetch: typeof fetch
+  }
 }
 
 export default {
   async fetch(request: Request, env: Env, ctx: ExecutionContext) {
     try {
-      const url = new URL(request.url);
+      const url = new URL(request.url)
 
       if (url.pathname === '/init.js') {
-        const assetRequest = new Request(`${url.origin}/public/init.js`, request);
-        const response = await env.ASSETS.fetch(assetRequest);
+        const assetRequest = new Request(
+          `${url.origin}/public/init.js`,
+          request
+        )
+        const response = await env.ASSETS.fetch(assetRequest)
 
-        const headers = new Headers(response.headers);
-        headers.set('X-Content-Type-Options', 'nosniff');
-        headers.set('Content-Type', 'application/javascript; charset=utf-8');
+        const headers = new Headers(response.headers)
+        headers.set('X-Content-Type-Options', 'nosniff')
+        headers.set('Content-Type', 'application/javascript; charset=utf-8')
 
-        headers.set('Access-Control-Allow-Origin', '*');
-        headers.set('Access-Control-Allow-Methods', 'GET, OPTIONS');
+        headers.set('Access-Control-Allow-Origin', '*')
+        headers.set('Access-Control-Allow-Methods', 'GET, OPTIONS')
         // cache for 24 hours
-        headers.set('Cache-Control', 'public, max-age=86400');
-        
+        headers.set('Cache-Control', 'public, max-age=86400')
+
         return new Response(response.body, {
           status: response.status,
           statusText: response.statusText,
           headers
-        });
+        })
       }
-      
+
       return new Response('Web Monetization Embed Script', {
         status: 200,
         headers: { 'Content-Type': 'text/plain' }
-      });
+      })
     } catch (error) {
-      return new Response(`Error: ${error}`, { status: 500 });
+      return new Response(`Error: ${error}`, { status: 500 })
     }
-  },
-};
+  }
+}
