2d3bc66070e16b05c5a957786524cff736d31d44
darianm <47156919+DarianM@users.noreply.github.com>
2025-05-09 14:07:30 +0300

    refactor!: move embed script and its hosting from frontend (#102)
    * embed module with the init.js script
    
    * separate worker to handle request for the embed script

---
+++ embed/vite.config.ts
@@ -0,0 +1,46 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig(({ mode }) => {
+  const isDevelopment = mode === 'development'
+
+  return {
+    envDir: resolve(__dirname),
+    build: {
+      lib: {
+        // entry point for the script file
+        entry: resolve(__dirname, 'src/index.ts'),
+        formats: ['iife'],
+        name: 'InitScript',
+        fileName: () => 'init.js'
+      },
+      outDir: resolve(__dirname, 'public'),
+      // don't clear the directory when building
+      emptyOutDir: false,
+      rollupOptions: {
+        output: {
+          entryFileNames: 'init.js',
+          // ensure all code is in one file
+          inlineDynamicImports: true,
+          compact: !isDevelopment,
+          manualChunks: undefined
+        },
+        external: []
+      },
+      cssCodeSplit: false,
+      minify: !isDevelopment,
+      watch: isDevelopment ? {} : null
+    },
+    server: {
+      cors: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      watch: {
+        // ensures file polling for file changes
+        usePolling: true,
+        ignored: ['node_modules/**', 'public/**']
+      }
+    }
+  }
+})
---
+++ worker/index.ts
@@ -0,0 +1,27 @@
+import initScript from '../embed/public/init.js?raw'
+
+export default {
+  fetch(request: Request) {
+    const url = new URL(request.url)
+
+    if (url.pathname === '/init.js') {
+      return new Response(initScript, {
+        headers: {
+          'Content-Type': 'application/javascript; charset=utf-8',
+          'X-Content-Type-Options': 'nosniff',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, OPTIONS',
+          // cache for 5 minutes
+          'Cache-Control': 'public, max-age=300'
+        }
+      })
+    }
+
+    return new Response(`Web Monetization Tools Worker`, {
+      status: 404,
+      headers: {
+        'Content-Type': 'text/plain'
+      }
+    })
+  }
+}
