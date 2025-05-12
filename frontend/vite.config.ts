efece32a8b9887429098b6dd2686b13297d07a3e
darian <darian410@gmail.com>
2025-05-12 13:39:28 +0300

    Revert "add frontend worker"
    This reverts commit dc2f8c2ef3d8e7ff90ce8d8d6d6e47cb39bc5694.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,12 @@ import {
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
+declare module '@remix-run/cloudflare' {
+  interface Future {
+    v3_singleFetch: true
+  }
+}
+
 export default defineConfig({
   plugins: [
     remixCloudflareDevProxy(),
