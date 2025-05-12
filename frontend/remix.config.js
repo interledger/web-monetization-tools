dc2f8c2ef3d8e7ff90ce8d8d6d6e47cb39bc5694
darian <darian410@gmail.com>
2025-05-12 12:41:20 +0300

    add frontend worker

--- frontend/remix.config.js
+++ frontend/remix.config.js
@@ -1,23 +1,23 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-export default {
-  serverBuildTarget: 'cloudflare-pages',
+module.exports = {
+  serverBuildTarget: 'cloudflare-workers',
+  server: './server.ts',
+  devServerBroadcastDelay: 1000,
   ignoredRouteFiles: ['**/.*'],
+
   appDirectory: 'app',
-  assetsBuildDirectory: 'public/build',
-  publicPath: '/build/',
+  assetsBuildDirectory: 'build/client',
+  publicPath: '/',
   serverBuildPath: 'build/server/index.js',
-  serverModuleFormat: 'esm',
-  serverPlatform: 'neutral',
-  routes(defineRoutes) {
-    return defineRoutes((route) => {
-      route('app/routes', '/')
-    })
-  },
+
   future: {
     v3_fetcherPersist: true,
     v3_relativeSplatPath: true,
     v3_throwAbortReason: true,
     v3_singleFetch: true,
     v3_lazyRouteDiscovery: true
-  }
+  },
+  serverModuleFormat: 'esm',
+  serverDependenciesToBundle: 'all',
+  vite: true
 }
