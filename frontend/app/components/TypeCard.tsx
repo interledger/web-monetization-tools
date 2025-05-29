314910f92c63d5097133a264a135c0a5d8b3f099
darian <darian410@gmail.com>
2025-05-29 11:39:51 +0300

    revert changes

--- frontend/app/components/TypeCard.tsx
+++ frontend/app/components/TypeCard.tsx
@@ -1,3 +1,4 @@
+import type { LinkProps } from '@remix-run/react'
 import { cx } from 'class-variance-authority'
 import { Button, InfoWithTooltip } from './index.js'
 import placeholderImg from '~/assets/images/placeholder.svg'
@@ -7,7 +8,7 @@ export type TypeCardProps = {
   title: string
   tooltip: string
   description: string
-  link: string
+  link: LinkProps['to'] | never
   bgColor: string
 }
 
@@ -19,11 +20,6 @@ export const TypeCard = ({
   link,
   bgColor
 }: TypeCardProps) => {
-  // TODO: to be removed once generators are fully migrated into tools
-  const shouldOpenInParent = ['/prob-revshare', '/link-tag'].some((path) =>
-    link.includes(path)
-  )
-
   return (
     <div className="flex flex-col shrink-0 bg-white rounded-lg w-80 p-6 border border-wm-green-shade">
       <div className={cx('flex py-6 rounded-lg bg-gradient-to-r', bgColor)}>
@@ -40,12 +36,7 @@ export const TypeCard = ({
       <p className="text-center text-sm min-h-36 p-4 mb-4 h-full">
         {description}
       </p>
-      <Button
-        intent="default"
-        aria-label={title}
-        to={link}
-        {...(shouldOpenInParent && { target: '_parent' })}
-      >
+      <Button intent="default" aria-label={title} to={link}>
         Generate
       </Button>
     </div>
