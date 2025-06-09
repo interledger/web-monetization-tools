7da7f8edab3eb7a9929616d7665cc5646f460c6b
darian <darian410@gmail.com>
2025-06-09 17:18:44 +0300

    do not enable

--- frontend/app/lib/presets.ts
+++ frontend/app/lib/presets.ts
@@ -135,7 +135,7 @@ export const availableTools = [
       "The banner informs visitors who don't have the Web Monetization extension active, with a call-to-action linking to the extension or providing details about the options available. It also adds your wallet address for your site to be monetized."
   },
   {
-    enabled: true,
+    enabled: false,
     title: 'Widget',
     image: widgetImg,
     bgColor: 'from-wm-red to-wm-red-fade',
