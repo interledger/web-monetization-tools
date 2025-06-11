e281d295e1af06897179f7dc12228157f9ae9a71
darian <darian410@gmail.com>
2025-06-11 18:47:11 +0300

    rmc unused concurrently; remove impocat eslint-plugin-import; update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,6 @@ import globals from 'globals'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
-import pluginImport from 'eslint-plugin-import'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -16,7 +15,6 @@ export default [
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginImport.flatConfigs.recommended,
   {
     ...pluginReact.configs.flat.recommended,
     settings: {
