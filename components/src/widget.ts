8f4eaceff7e5b1b91dd421c68c34028a858f0727
darian <darian410@gmail.com>
2025-06-12 17:54:21 +0300

    icon

--- components/src/widget.ts
+++ components/src/widget.ts
@@ -8,7 +8,7 @@ import {
 import { property, state } from 'lit/decorators.js'
 import './confirmation.js'
 import './interaction.js'
-import defaultTriggerIcon from './assets/wm_logo_animated.svg'
+import { defaultTriggerIcon } from './assets/default-icon.js'
 
 export interface WidgetConfig {
   walletAddress: string
