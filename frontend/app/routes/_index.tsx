7c88c79c1b6818f9072867b1ae307496768dc01d
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2025-06-12 19:28:47 +0530

    enable or disable widget based on `?widget=1` url param

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,4 +1,6 @@
-import type { MetaFunction } from '@remix-run/cloudflare'
+import { json } from '@remix-run/cloudflare'
+import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/cloudflare'
+import { useLoaderData } from '@remix-run/react'
 import { TypeCard } from '~/components/index.js'
 import { availableTools } from '~/lib/presets.js'
 
@@ -8,13 +10,24 @@ export const meta: MetaFunction = () => {
     { name: 'description', content: 'Choose and configure your elements!' }
   ]
 }
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const enableWidget = new URL(request.url).searchParams.get('widget') === '1'
+  return json({ enableWidget })
+}
+
 export default function Index() {
+  const { enableWidget } = useLoaderData<typeof loader>()
   return (
     <div className="h-full p-14">
       <div className="flex flex-wrap justify-center gap-6 max-w-prosex p-4 mx-auto">
-        {availableTools.map((tool) => (
-          <TypeCard key={tool.link} {...tool} />
-        ))}
+        {availableTools
+          .filter(
+            (tool) => tool.enabled || (tool.link === 'widget' && enableWidget)
+          )
+          .map((tool) => (
+            <TypeCard key={tool.link} {...tool} />
+          ))}
       </div>
     </div>
   )
