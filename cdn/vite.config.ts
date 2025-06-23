9ab9da137fae1743c92e02c0a0248993a73596f8
Sid Vishnoi <8426945+sidvishnoi@users.noreply.github.com>
2025-06-23 21:01:10 +0530

    chore(cdn): restart cdn dev server on embed script generate

--- cdn/vite.config.ts
+++ cdn/vite.config.ts
@@ -1,6 +1,26 @@
+import * as path from 'node:path'
 import { defineConfig } from 'vite'
 import { cloudflare } from '@cloudflare/vite-plugin'
+import viteRestart from 'vite-plugin-restart'
+
+// https://github.com/antfu/vite-plugin-restart/issues/10#issuecomment-1155859677
+const restartOnNodeModuleChange = ['publisher-tools-embed/dist/init.js']
 
 export default defineConfig({
-  plugins: [cloudflare()]
+  plugins: [
+    cloudflare(),
+    viteRestart({
+      restart: restartOnNodeModuleChange.map((e) => `node_modules/${e}`)
+    })
+  ],
+  server: {
+    watch: {
+      ignored: restartOnNodeModuleChange.map(
+        (e) => `!${path.join(__dirname, `node_modules/${e}`)}`
+      )
+    }
+  },
+  optimizeDeps: {
+    exclude: [...restartOnNodeModuleChange]
+  }
 })
