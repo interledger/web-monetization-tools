43268a8106a6ad8e5a96ca1f747f1301cd6daf64
Kenneth Mitchell <kenneth@interledger.org>
2025-06-25 00:18:56 -0400

    Initial footer component per design specs

---
+++ frontend/app/components/redesign/components/Footer.tsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+  return (
+    <footer className="w-full pb-8">
+      <div className="container mx-auto px-4 py-px text-center">
+        <span className="text-silver-800 text-sm font-normal font-sans leading-5">
+          Copyright© 2024 Interledger Foundation.
+        </span>
+      </div>
+    </footer>
+  )
+}
