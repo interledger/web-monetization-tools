afe45c9bebd8828b76f78bfc646897317f1a642c
Kenneth Mitchell <kenneth@interledger.org>
2025-06-26 17:25:50 -0400

    Remove unnecessary div

--- frontend/app/components/redesign/components/NavBar.tsx
+++ frontend/app/components/redesign/components/NavBar.tsx
@@ -9,16 +9,14 @@ export const NavBar = () => {
       className="w-full bg-white shadow-[0px_12px_20px_0px_rgba(0,0,0,0.06)] gap-2.5"
     >
       <div className="max-w-7xl mx-auto px-md py-xs flex justify-between items-center">
-        <div className="pl-md">
-          {/* Web Monetization Logo */}
-          <RemixNavLink to="/">
-            <img
-              src={wmLogo}
-              alt="Web Monetization Logo"
-              className="lg:w-11 lg:h-11"
-            />
-          </RemixNavLink>
-        </div>
+        {/* Web Monetization Logo */}
+        <RemixNavLink to="/">
+          <img
+            src={wmLogo}
+            alt="Web Monetization Logo"
+            className="lg:w-11 lg:h-11"
+          />
+        </RemixNavLink>
         <ul className="flex gap-md pr-md">
           <NavDropdown title="Tools"></NavDropdown>
           <NavLink to="/docs">Documentation</NavLink>
