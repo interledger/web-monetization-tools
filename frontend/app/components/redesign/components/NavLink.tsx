34edc8b7dc5437f3aa1793a01a3a702b935dbb61
Kenneth Mitchell <kenneth@interledger.org>
2025-06-26 13:20:46 -0400

    Finishing touches on styling for the header

--- frontend/app/components/redesign/components/NavLink.tsx
+++ frontend/app/components/redesign/components/NavLink.tsx
@@ -10,13 +10,13 @@ export const NavLink = ({ to, children }: NavLinkProps) => {
   return (
     <li
       className={
-        'group px-md py-sm rounded-lg inline-flex justify-center items-center gap-2.5 hover:bg-secondary-hover-surface'
+        'group rounded-lg inline-flex justify-center items-center gap-2.5 hover:bg-secondary-hover-surface'
       }
     >
       <RemixNavLink
         to={to}
         className={
-          'font-sans font-normal text-sm leading-5 text-purple-300 group-hover:text-secondary-edge-hover'
+          'font-sans font-normal text-sm leading-5 text-purple-300 group-hover:text-secondary-edge-hover px-md py-sm'
         }
       >
         {children}
